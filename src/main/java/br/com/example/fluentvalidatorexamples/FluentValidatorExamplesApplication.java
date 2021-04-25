be9a4c4ab0ab47d7524bbd246acb488246fa9662
Paulo Sergio Pereira Junior <paulosergio.jnr@gmail.com>
2021-04-25 11:06:05 -0300

    chore: created basic Spring Application

---
+++ src/main/java/br/com/example/fluentvalidatorexamples/FluentValidatorExamplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.fluentvalidatorexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluentValidatorExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluentValidatorExamplesApplication.class, args);
+	}
+
+}
